5d74fa617a527f7246b42b18161ddd21d33abf62
Oris John-Baptiste <orisjb@gmail.com>
2019-07-12 10:17:14 +0100

    removed react logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SimpleExpansionPanel from './accordion';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Oris' React Table</h1>
         </header>
        <SimpleExpansionPanel/>
